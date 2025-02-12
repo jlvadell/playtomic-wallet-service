a25f917fa527f1fa6cc6e724d5a8a8c875f1caa1
José Vadell <me@jlvadell.com>
2025-02-12 16:11:33 +0100

    feat: define hexagonal architecture skeleton

--- exercise-wallet-boot/src/main/java/com/playtomic/tests/wallet/service/StripeRestTemplateResponseErrorHandler.java
+++ exercise-wallet-boot/src/main/java/com/playtomic/tests/wallet/service/StripeRestTemplateResponseErrorHandler.java
@@ -1,6 +1,7 @@
 package com.playtomic.tests.wallet.service;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 
@@ -9,7 +10,7 @@ import java.io.IOException;
 public class StripeRestTemplateResponseErrorHandler extends DefaultResponseErrorHandler {
 
     @Override
-    protected void handleError(ClientHttpResponse response, HttpStatus statusCode) throws IOException {
+    protected void handleError(ClientHttpResponse response, HttpStatusCode statusCode) throws IOException {
         if (statusCode == HttpStatus.UNPROCESSABLE_ENTITY) {
             throw new StripeAmountTooSmallException();
         }
