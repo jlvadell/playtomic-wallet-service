adb5d31ba12c8d32a82987d9ba4249c33c1c1a54
José Vadell <me@jlvadell.com>
2025-02-14 16:34:57 +0100

    style: use Dto suffix for generated api models

--- exercise-wallet-contract/src/main/java/com/playtomic/tests/contract/restapi/WalletController.java
+++ exercise-wallet-contract/src/main/java/com/playtomic/tests/contract/restapi/WalletController.java
@@ -2,9 +2,9 @@ package com.playtomic.tests.contract.restapi;
 
 
 import com.playtomic.tests.contract.restapi.wallet.WalletApi;
-import com.playtomic.tests.contract.restapi.wallet.model.Transaction;
-import com.playtomic.tests.contract.restapi.wallet.model.TransactionRequest;
-import com.playtomic.tests.contract.restapi.wallet.model.Wallet;
+import com.playtomic.tests.contract.restapi.wallet.model.TransactionDto;
+import com.playtomic.tests.contract.restapi.wallet.model.TransactionRequestDto;
+import com.playtomic.tests.contract.restapi.wallet.model.WalletDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class WalletController implements WalletApi {
 
     @Override
-    public ResponseEntity<Transaction> addTransaction(String walletId, TransactionRequest transactionRequest) {
-        return WalletApi.super.addTransaction(walletId, transactionRequest);
+    public ResponseEntity<TransactionDto> addTransaction(String walletId, TransactionRequestDto transactionRequestDto) {
+        return WalletApi.super.addTransaction(walletId, transactionRequestDto);
     }
 
     @Override
-    public ResponseEntity<Wallet> findWalletById(String walletId) {
+    public ResponseEntity<WalletDto> findWalletById(String walletId) {
         return WalletApi.super.findWalletById(walletId);
     }
 }
